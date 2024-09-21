02f5d703e07ec10e9a1e152355f230fe73b440ee
LuisEAL0 <zluisalvaa@gmail.com>
2024-09-20 21:42:07 -0600

    Add tree class & add filtering by index

--- DecisionTree/decisionTree.py
+++ DecisionTree/decisionTree.py
@@ -1,10 +1,29 @@
 from math import log2
 from math import inf
 
+# https://stackoverflow.com/a/28015122
+class Tree(object):
+    "Generic tree node."
+    def __init__(self, name='root', children=None, data=None):
+        self.name = name
+        self.data = data
+        self.children = []
+        if children is not None:
+            for child in children:
+                self.add_child(child)
+
+    def __repr__(self):
+        return self.name
+    
+    def add_child(self, node):
+        assert isinstance(node, Tree)
+        self.children.append(node)
+
 def bool_table1(x2, x4):
     return x4 & ~x2
 
 def entropy(k_labels):
+    k_labels.pop("filter", None)
     values = k_labels.values()
     total = 0
     for k in values:
@@ -26,10 +45,14 @@ def info_gain(total_entropy, attribute, labels):
     total_labels = total_entropy[1]
     for subset in subsets:
         subset_labels = subsets[subset]
+        temp_sub = subsets[subset]['filter']
+
         res = entropy(subset_labels)
         gain -= (res[1] / total_labels) * res[0]
+        
+        subsets[subset]['filter'] = temp_sub
     
-    return gain
+    return (gain, subsets)
 
 def create_subsets(attribute, labels):
     subsets = {}
@@ -37,10 +60,11 @@ def create_subsets(attribute, labels):
 
     for item in attribute:
         if item not in subsets:
-            subsets[item] = {}
+            subsets[item] = {"filter":[]}
             for label in labels:
                 subsets[item][label] = 0
-                
+        
+        subsets[item]["filter"].append(index)
         subsets[item][labels[index]] += 1
         index += 1
 
@@ -60,17 +84,21 @@ def split(attributes, labels):
 
     best_attribute = ''
     max_gain = -inf
+    subsets_attribute = {}
     for attribute in attributes:
-        gain = info_gain(entropy(label_counts), attributes[attribute], labels)
-        if info_gain(entropy(label_counts), attributes[attribute], labels) > max_gain:
+        res = info_gain(entropy(label_counts), attributes[attribute], labels)
+        gain = res[0]
+        subset = res[1]
+        if gain > max_gain:
             best_attribute = attribute
             max_gain = gain
+            subsets_attribute = subset
     
-    return best_attribute
-
+    return (best_attribute, subsets_attribute)
 
 def ID3(s, attributes, label):
-    ...
+    labels = create_label(s)
+    count = len(s)
 
 def main():
     data = {
@@ -82,7 +110,7 @@ def main():
     labels = {
         "y": [0, 0, 1, 1, 0, 0, 0]
     }
-    split(data, labels["y"])
+    ID3(labels["y"], data, [0, 1])
 
 if __name__ == '__main__':
     main()
