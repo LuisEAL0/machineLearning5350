65f1a4fb7bf867eb9c86b9a7c6e1fed10e78d37a
LuisEAL0 <zluisalvaa@gmail.com>
2024-10-19 12:14:44 -0600

    Add reading file

--- LinearRegression/linearRegression.py
+++ LinearRegression/linearRegression.py
@@ -1,5 +1,6 @@
 from math import inf
 from math import sqrt
+import csv
 
 class LinearRegression:
     ''' Linear Regression model to make predictions based on given data '''
@@ -31,7 +32,6 @@ class LinearRegression:
         loss : integer
             total loss of the prediction
         """
-
         self.values = values
         self.labels = labels
         self.prediction = [0] * len(self.labels)
@@ -41,7 +41,6 @@ class LinearRegression:
     
     def createPrediction(self):
         '''Creates a prediction vector using the self values and weight vectors'''
-
         for i in range(len(self.values)):
             self.prediction[i] = 0
             for j in range(len(self.weight)):
@@ -52,7 +51,6 @@ class LinearRegression:
     def updateLoss(self):
         '''Calculates the total loss of current prediction'''
         loss = 0
-
         for i in range(len(self.labels)):
             loss += (self.labels[i] - self.prediction[i]) ** 2
         self.loss = .5 * loss
@@ -68,26 +66,33 @@ class LinearRegression:
 
         return self.gradient
     
-    def batchGradient(self, r):
+    def batchGradient(self, r, errorFunc):
         '''
         Batch Gradient Algorithm
 
-        If loss grows to large will return latest prediction 
+        If loss grows to large will return latest weight 
         
         Parameters
         ----------
         r : float
             learning rate
+        errorFunc : function
+            function to calculate error between current and next weight
+        
+        Returns
+        -------
+        weight vector used to make predictions
         '''
-
         t = 0
+        maxLoops = 1000000
         epochWeight = [0] * len(self.weight)
         weightDifference = [1] * len(self.weight)
         
-        while(euclidNorm(weightDifference) > (10 ** -6)):
+        while(errorFunc(weightDifference) > (10 ** -6) or t > maxLoops):
             self.createPrediction()
             self.updateLoss()
-            if self.loss > (10 ** 20):
+            if self.loss > (10 ** 100):
+                print("No convergence... Pick different r")
                 break
 
             self.updateGradient()
@@ -97,7 +102,7 @@ class LinearRegression:
             self.weight = epochWeight.copy()
             t += 1
         
-        return self.prediction
+        return self.weight
             
 def euclidNorm(vector):
     '''Calculates the Euclidean Norm of a vector'''
@@ -107,13 +112,54 @@ def euclidNorm(vector):
     
     return sqrt(norm)
 
+def read_csv_line_prepend_bias(filepath, attributes : list):
+    '''
+    Creates a values matrix (prepended with 1) and labels vector based off CSV file.
+    Assumes final column are labels. 
+
+    Parameters
+    ----------
+    filepath : string
+        path to file
+    attributes : list
+        column names with last column assumed to be named 'Output'
+    
+    Returns
+    -------
+    Tuple containing the values matrix in 0th index and labels vector 1st index
+    '''
+    values = []
+    labels = []
+    with open(filepath, mode='r') as file:
+        csv_reader = csv.DictReader(file, attributes)
+        for row in csv_reader:
+            tempV = [1]
+            for attribute in attributes:
+                if attribute != "Output":
+                    tempV.append(float(row[attribute]))
+                else:
+                    labels.append(float(row[attribute]))
+            values.append(tempV)
+
+    return [values, labels]
+
 def main():
-    lr = LinearRegression([[1, 31.5, 6],
-                           [1, 36.2, 2],
-                           [1, 43.1, 0],
-                           [1, 27.6, 2]], [21, 25, 18, 30])
-    print(lr.batchGradient(.0001))
-    print(lr.weight)
+    traindata = read_csv_line_prepend_bias("./LinearRegression/concreteData/train.csv", ["Cement", "Slag", "Fly ash", "Water", "SP", "Coarse Aggr", "Fine Aggr", "Output"])
+    testdata = read_csv_line_prepend_bias("./LinearRegression/concreteData/test.csv", ["Cement", "Slag", "Fly ash", "Water", "SP", "Coarse Aggr", "Fine Aggr", "Output"])
+
+    lr = LinearRegression(traindata[0], traindata[1])
+    test_lr = LinearRegression(testdata[0], testdata[1])
+    test_lr.weight = lr.batchGradient(.001, euclidNorm).copy()
+
+    # print(lr.createPrediction())
+    # print(lr.updateLoss())
+
+    print(test_lr.createPrediction())
+    print(test_lr.labels)
+    print(test_lr.updateLoss())
+
+    # print(max(traindata[1]))
+    # print(min(traindata[1]))
 
 if __name__ == "__main__":
     main()
