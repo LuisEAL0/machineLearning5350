15a88ce7360be0f9179a40bfddaf3508fef756cf
LuisEAL0 <zluisalvaa@gmail.com>
2024-11-07 12:36:38 -0700

    Set up perceptron files

---
+++ Perceptron/perceptron.py
@@ -0,0 +1,14 @@
+import pandas
+import numpy
+
+class Perceptron:
+    ''' TODO: Create Perceptron algorithm'''
+    
+    def __init__(self):
+        pass
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    main()
